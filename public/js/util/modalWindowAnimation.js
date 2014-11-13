cedae41653049d376b837ad4d60aaa1a90a5b2f0
Saint1991 <se-mizuno@hotmail.co.jp>
2014-11-14 03:45:10 +0900

    モーダルウィンドウの試作

---
+++ public/js/util/modalWindowAnimation.js
@@ -0,0 +1,16 @@
+
+
+$(funciton() {
+
+	$('#room-create').click(
+	
+		function() {
+			$('body').append('<div id="overlay"></div>');
+			$('#overlay').fadeIn('slow');
+			$('#modal-window').fadeIn('slow');	
+		}
+
+	);
+
+})
+
