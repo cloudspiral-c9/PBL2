3668ac620a7f06acd32e319990ec49275d2f5616
Saint1991 <se-mizuno@hotmail.co.jp>
2014-11-13 22:45:10 +0900

    cssをaddした

--- app/socket/socket-csbs/csbsChart.js
+++ app/socket/socket-csbs/csbsChart.js
@@ -7,7 +7,7 @@ var TimestampHelper = require(__dirname + '/../../services/util/TimestampHelper.
 var def = require('deferred');
 var csbs = require('csbind-server');
 
-var csbsChart;
+var csbsChart = {};
 
 (function() {
 
